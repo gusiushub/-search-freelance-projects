34a11490bde3445d241aded80f07ccb5bb3291b0
nikita <zolowkin.nikita@yandex.ru>
2018-11-07 12:17:51 +0300

    Исправляем функционал для работы с интеркассой

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -230,19 +230,19 @@ class UserController extends Controller
         ])->execute();
 
 
-$user = User::find()->where(['like','id',Yii::$app->user->id])->one();
-$user->paid_id=time() + 2678400;
-$user->save(false);
+//$user = User::find()->where(['like','id',Yii::$app->user->id])->one();
+//$user->paid_id=time() + 2678400;
+//$user->save(false);
 
 //            try {
                 if (Yii::$app->user->identity->paid_to==0 or Yii::$app->user->identity->paid_to < time()) {
 
                     $paid_to = time() + 2678400;
-                    Yii::$app->db->createCommand()->update('user', ['paid_to' => $paid_to], 'id =:id',[ ':id'=> Yii::$app->user->id])->execute();
+                    return Yii::$app->db->createCommand()->update('user', array('paid_to' => $paid_to), 'id =:id',array( ':id'=> Yii::$app->user->id))->execute();
                 }elseif(Yii::$app->user->identity->paid_to!=0 and Yii::$app->user->identity->paid_to>time()){
                     $paid_to = Yii::$app->user->identity->paid_to;
                     $paid_to = $paid_to+2678400;
-                    Yii::$app->db->createCommand()->update('user', ['paid_to' => $paid_to], 'id =:id',[ ':id'=> Yii::$app->user->id])->execute();
+                    return Yii::$app->db->createCommand()->update('user', array('paid_to' => $paid_to), 'id =:id',array( ':id'=> Yii::$app->user->id))->execute();
                 }
 
 //        file_put_contents('1.txt', "Логин: $_POST[ik_x_id], сум: $_POST[ik_am]");
