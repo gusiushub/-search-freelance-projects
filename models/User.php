3f654ac215ba62ca9d86c0290864eb2720d5ca17
nikita <zolowkin.nikita@yandex.ru>
2018-11-07 13:28:10 +0300

    Исправляем функционал для работы с интеркассой

--- models/User.php
+++ models/User.php
@@ -88,6 +88,7 @@ class User extends ActiveRecord  implements IdentityInterface
         return [
             ['status', 'default', 'value' => self::STATUS_ACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            [['payment_status'],'required'],
 //            ['paid_to', 'integer']
         ];
     }
